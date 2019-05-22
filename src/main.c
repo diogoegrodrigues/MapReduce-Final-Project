6f46d2aba63a5afd204b486a6660ef221b8ee998
krunnman <krunnman@kth.se>
2019-05-23 01:38:40 +0200

    Tokenized the text

--- src/main.c
+++ src/main.c
@@ -3,11 +3,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <mpi.h>
+#include <ctype.h>
 
 #define CHUNK_SIZE 64
 
+char** read_file();
+void tokenize();
 void print_usage();
-void read_file();
 
 int main(int argc, char *argv[]) {
 	int opt, world_rank, num_ranks;
@@ -30,40 +32,53 @@ int main(int argc, char *argv[]) {
 				if (world_rank == 0) print_usage(argv[0]);
 		}
 	}
-  read_file(filename, world_rank, num_ranks);
+  char** text = read_file(filename, world_rank, num_ranks);
+	tokenize(text);
+	free(text);
 
-	MPI_Finalize();
+	MPI_Finalize();printf("%s\n", text[0]);
   if(world_rank == 0 ) {
-    if(filename != NULL) {
-      printf("filepath: %s \nrepeat: %d \n", filename, repeat);
-    } else {
+    if(filename == NULL) {
       print_usage(argv[0]);
     }
   }
 	return 0;
 }
 
-void read_file(char *filename, int rank, int num_ranks) {
+char** read_file(char *filename, int rank, int num_ranks) {
   MPI_File fh;
-  char *buffer = (char*)malloc(CHUNK_SIZE*sizeof(char));
+	MPI_Offset filesize;
+	MPI_File_open(MPI_COMM_WORLD, filename, MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
+	MPI_File_get_size(fh, &filesize);
   MPI_Aint length = CHUNK_SIZE * sizeof(int);
-  MPI_Aint extent = num_ranks * length;
   MPI_Offset disp = rank * length;
-  MPI_Offset filesize;
-  MPI_Datatype contig, filetype;
 
-  MPI_Type_contiguous(CHUNK_SIZE, MPI_INT, &contig);
-  MPI_Type_create_resized(contig, 0, extent, &filetype);
-  MPI_Type_commit(&filetype);
+	int nblocks = (int) filesize / num_ranks;
+	char **result = (char**)malloc(nblocks*sizeof(char*));
+	for(int i = 0; i < nblocks; i++) {
+		result[i] = (char*)malloc(CHUNK_SIZE*sizeof(char));
+	}
 
-  MPI_File_open(MPI_COMM_WORLD, filename, MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
-  MPI_File_get_size(fh, &filesize);
-  MPI_File_set_view(fh, disp, MPI_INT, filetype, "native", MPI_INFO_NULL);
-  MPI_File_read_all(fh, buffer, CHUNK_SIZE, MPI_INT, MPI_STATUS_IGNORE);
+	for(int i = 0; i < nblocks; i++) {
+		MPI_File_seek(fh, disp, MPI_SEEK_SET);
+  	MPI_File_read(fh, result[i], CHUNK_SIZE, MPI_INT, MPI_STATUS_IGNORE);
+		disp += num_ranks * length;
 
-  printf("rank: %d\nbuffer: %s\n", rank, buffer);
-  printf("\n");
-  free(buffer);
+	}
+	return result;
+}
+
+void tokenize(char **text_array) {
+	for(int i = 0; i < sizeof(text_array); i++) {
+		for (char *p = text_array[i]; *p; p++)
+    	if (!isalpha(*p)) *p = ' ';
+
+		char* token = strtok(text_array[i], " ");
+		while (token) {
+    	printf("token: %s\n", token);
+    	token = strtok(NULL, " ");
+		}
+	}
 }
 
 void print_usage(char *program) {
